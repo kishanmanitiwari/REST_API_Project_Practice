dcce66296fb026bb8ce3113898a07a4ea095be30
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-06-10 16:07:10 +0530

    Added POST Route to API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import users from './MOCK_DATA .json' assert{type:'json'};
+import users from './MOCK_DATA.json' assert{type:'json'};
 import path from 'path';
+import fs from 'fs';
 
 
 const app = express();
@@ -29,10 +30,30 @@ app.get('/api/users',(req,res)=>{
 
 //Dynamic routing
 app.get('/api/users/:id',(req,res)=>{
-    // console.log(req.params);
     const id = Number(req.params.id);
     const user = users.find((user)=>user.id==id);
     res.json(user)
+})
+
+//POST Route
+
+app.post('/api/users',(req,res)=>{
+    //create new user
+    const newUser = req.body;
+    newUser.id = users.length+1;
+
+    //push this user
+    users.push(newUser);
+
+    //Write upadated users array to Mock_data.json
+   fs.writeFile(__dirname+'/MOCK_DATA.json',JSON.stringify(users),(err)=>{
+    if (err) {
+        console.log(err);
+        return res.status(50).json({status:"Failed to write file"});
+    } 
+    return res.status(201).json({status:"created"});
+   })
+
 })
 
 
