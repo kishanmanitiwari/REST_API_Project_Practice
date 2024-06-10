470f0463ac5d1a2c99f119384ec050f91de3e408
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-06-10 15:17:32 +0530

    Added Get Route

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from 'express';
-
+import users from './MOCK_DATA .json' assert{type:'json'};
+import path from 'path';
 
 
 const app = express();
 const PORT = 3000;
+const __dirname = path.resolve('./');
 
 
 
+//Routes
 
+app.get('/users',(req,res)=>{
+    const html = `<ul> 
+    ${users.map(user => `<li> ${user.first_name}</li>`).join('')}
+    </ul>`
+
+    res.send(html);
+})
+
+app.get('/api/users',(req,res)=>{
+    res.json(users);
+})
 
 
 
@@ -20,5 +34,5 @@ const PORT = 3000;
 //Server setup
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 })
