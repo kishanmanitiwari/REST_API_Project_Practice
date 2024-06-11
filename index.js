2fb2b1702ab841c432a4ade5e2f9049c14f94e06
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-06-11 12:31:04 +0530

    Implemented & Learrned Spread Operator

--- index.js
+++ index.js
@@ -49,13 +49,49 @@ app.post('/api/users',(req,res)=>{
    fs.writeFile(__dirname+'/MOCK_DATA.json',JSON.stringify(users),(err)=>{
     if (err) {
         console.log(err);
-        return res.status(50).json({status:"Failed to write file"});
+        return res.status(500).json({status:"Failed to write file"});
     } 
     return res.status(201).json({status:"created"});
    })
 
 })
 
+//PUT Route - HW
+
+app.put('/api/users',(req,res)=>{
+    //Write your put code over here
+})
+
+//Patch - Update
+//Find the user to update
+///api/users/100 -> 100 find
+
+app.patch('/api/users/:id', (req,res)=>{
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user)=> user.id == id);
+    
+    if(userIndex == -1){
+        return res.status(404).json({error:'User not found'});
+    }
+
+    const updatedUser = {...users[userIndex], ...req.body};
+    users[userIndex] = updatedUser;
+
+    //Sync
+    try {
+        fs.writeFileSync(path.join(__dirname,'MOCK_DATA.json'),JSON.stringify(users))
+        return res.json({status:'User sucessfully updated', user:updatedUser})
+    } catch (error) {
+        return res.status(505).json({error: 'Failed to update the user'})
+    }
+
+})
+
+
+
+
+
+//Delete - 
 
 
 
---
+++ spread.js
@@ -0,0 +1,20 @@
+
+// const ob1 = {a:1,b:3};
+// const obj2 = {...ob1};
+
+//console.log(obj2); //1 3
+
+//Merge
+
+// const obj1 = {a:1,b:2};
+// const obj2 = {c:3,d:4};
+// const ans = {...obj1,...obj2}
+// console.log(ans); //
+
+
+//Overwirte
+
+const obj1 = {a:1,b:2};
+const obj2 = {a:3,b:4};
+const ans = {...obj2,...obj1}
+console.log(ans); //
